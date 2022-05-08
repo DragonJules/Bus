46353b2b0be550c32bd98f721de821e2042ae404
ItzDragon <65538436+DragonJules@users.noreply.github.com>
2022-05-08 19:39:47 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function getUserPosition() {
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+
+    function success(pos) {
+        var crd = pos.coords;
+        automode(crd)
+    }
+
+    function error(err) {
+        console.warn(`ERREUR (${err.code}): ${err.message}`);
+    }
+    navigator.geolocation.getCurrentPosition(success, error, options);
+}
+
+
+function automode(crd) {
+    window.location.href = "./geomode.html"
+    alert(`Latitude : ${crd.latitude} Longitude : ${crd.longitude} La précision est de ${crd.accuracy} mètres.`)
+}
